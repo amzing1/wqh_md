c7854df9d63a167bffbbd1f479f0899db7ee821f
wuqinghua <1632726788@qq.com>
2022-02-13 23:24:24 +0800

    滑动窗口

--- 算法相关/labuladong/src/index.ts
+++ 算法相关/labuladong/src/index.ts
@@ -1,5 +1,6 @@
-import { corpFlightBookings } from "../差分数组/1109";
-import { getModifiedArray } from "../差分数组/370";
+import { solute } from "../滑动窗口/76";
 
-const res = corpFlightBookings([[1,2,10], [2,3,20], [2,5,25]], 5)
-console.log(res)
+const s = "ADOBECODEBANC";
+const t = "ABC";
+const res = solute(s, t);
+console.log(res);
---
+++ 算法相关/labuladong/滑动窗口/76.ts
@@ -0,0 +1,56 @@
+function hasFindAll(map: Map<string, number>) {
+  let res = true;
+  console.log(map);
+  map.forEach((value) => {
+    if (value > 0) {
+      res = false;
+    }
+  });
+  return res;
+}
+export function solute(s: string, t: string): string {
+  const map = new Map<string, number>();
+  for (let i = 0, l = t.length; i < l; i++) {
+    map.set(t[i], (map.get(t[i]) || 0) + 1);
+  }
+
+  let left = 0,
+    right = 0;
+  const length = s.length;
+  let toRight = true;
+  let minLen = Infinity;
+  let minStr = "";
+  while (right <= length) {
+    if (toRight) {
+      if (map.has(s[right])) {
+        map.set(s[right], map.get(s[right]) - 1);
+      }
+      if (!hasFindAll(map)) {
+        right++;
+      } else {
+        toRight = false;
+        right++;
+      }
+    } else {
+      if (map.has(s[left])) {
+        const value = map.get(s[left]);
+        if (value === 0) {
+          const dis = right - left;
+          if (dis < minLen) {
+            minStr = s.slice(left, right);
+            minLen = dis;
+          }
+          toRight = true;
+        } else if (value < 0) {
+          map.set(s[left], value + 1);
+          left++;
+        } else {
+          throw Error("error");
+        }
+      } else {
+        left++;
+      }
+    }
+  }
+  return minStr;
+}
